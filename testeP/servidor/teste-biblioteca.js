05e80818d379eff43a80e18147c36467f675f8cc
Andrew Barbosa Deras <andrewderas7@gmail.com>
2023-02-10 10:14:06 -0300

    começo do m5

---
+++ testeP/servidor/teste-biblioteca.js
@@ -0,0 +1,11 @@
+import chalk from 'chalk';
+// console.log(chalk.red('Olá mundo'));
+
+function temperaturaPaciente(temperatura) {
+  temperatura >= 37.5
+    ? console.log(chalk.red('Paciente febril'))
+    : console.log(chalk.green('Paciente não febril'))
+
+};
+
+temperaturaPaciente(37.4);
